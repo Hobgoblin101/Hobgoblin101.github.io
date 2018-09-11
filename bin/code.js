699fb241034e589f6e12ef9d88b267b7b82e5dc0
Ajani Bilby <aj@bilby.net>
2018-09-11 20:59:12 +1000

    ~fixed < within JS encoding

--- bin/code.js
+++ bin/code.js
@@ -1,212 +1,212 @@
 let letters = 'abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 function isLetter(char){
-  return letters.indexOf(char) != -1;
+	return letters.indexOf(char) != -1;
 }
 
 
 const math = require('./math.js');
 let highlight = {
-  typ: [ //Type definition
-    'function ', 'func', 'def',
-    'var', 'let', 'constructor',
-    'indexOf', 'class'
-  ],
-  kwd: [ // Keywords
-    'define', 'const', 'delete',
-    'return', 'yeild', 'throw',
-    'if', 'else', 'elif', 'for',
-    'continue', 'break',
-    'in', 'of', 'is', 'not', 'and', 'or',
-    'select', 'from', 'where', 'orderby', 'join',
-    'import',
-    '!', '=', '+', '/ ', '%', '*', '-', '&', '|',
-    '&gt', '&lt', //'<', '>'
-    'new'
-  ],
-  lit: [ // Literals
-    '\\',
-    'true', 'false',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-  ],
-  spe: [
-    'this'
-  ],
-  cal: [
-  ]
+	typ: [ //Type definition
+		'function ', 'func', 'def',
+		'var', 'let', 'constructor',
+		'indexOf', 'class'
+	],
+	kwd: [ // Keywords
+		'define', 'const', 'delete',
+		'return', 'yeild', 'throw',
+		'if', 'else', 'elif', 'for',
+		'continue', 'break',
+		'in', 'of', 'is', 'not', 'and', 'or',
+		'select', 'from', 'where', 'orderby', 'join',
+		'import',
+		'&gt', '&lt', //'<', '>'
+		'!', '=', '+', '/ ', '%', '*', '-', '&', '|',
+		'new'
+	],
+	lit: [ // Literals
+		'\\',
+		'true', 'false',
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+	],
+	spe: [
+		'this'
+	],
+	cal: [
+	]
 }
 
 function encode(input){
-  let i = input.indexOf('\n') || 0;
-  let type = input.slice(0, i);
-  input = input.slice(i+1);
+	let i = input.indexOf('\n') || 0;
+	let type = input.slice(0, i);
+	input = input.slice(i+1);
 
-  if (type == 'plain'){
-    let res = input.replace(/</g, '&lt').replace(/>/g, '&gt') // Encode <, >
-      .replace(/\n/g, '<br>').replace(/ /g, '&nbsp').replace(/\t/g, '&nbsp&nbsp'); // Encode tabs and spaces
+	if (type == 'plain'){
+		let res = input.replace(/</g, '&lt').replace(/>/g, '&gt') // Encode <, >
+			.replace(/\n/g, '<br>').replace(/ /g, '&nbsp').replace(/\t/g, '&nbsp&nbsp'); // Encode tabs and spaces
 
-    return `<code>${res}</code>`
-  }else if (type == 'math'){
-    return '<code type="eq">'+math.encode(input)+'</code>';
-  }
+		return `<code>${res}</code>`
+	}else if (type == 'math'){
+		return '<code type="eq">'+math.encode(input)+'</code>';
+	}
 
-  let res = input.slice(0, -1).replace(/</g, '&lt').replace(/>/g, '&gt') // Encode <, >
-  let j;
+	let res = input.slice(0, -1).replace(/</g, '&lt').replace(/>/g, '&gt') // Encode <, >
+	let j;
 
-  loop: for (i=0; i<res.length; i++){
-    // Color single line comments
-    if (res[i] == '/' && res[i+1] == '/'){
-      
-      scan: for (j=i+2; j<res.length; j++){
-        if (res[j] === '\n'){
-          break scan;
-        }
-      }
+	loop: for (i=0; i<res.length; i++){
+		// Color single line comments
+		if (res[i] == '/' && res[i+1] == '/'){
+			
+			scan: for (j=i+2; j<res.length; j++){
+				if (res[j] === '\n'){
+					break scan;
+				}
+			}
 
-      let insert = `<span class="com">${res.slice(i,j)}</span>`;
-      res = res.slice(0, i) + insert + res.slice(j);
+			let insert = `<span class="com">${res.slice(i,j)}</span>`;
+			res = res.slice(0, i) + insert + res.slice(j);
 
-      i += insert.length-1; //Don't scan within the comment
-      continue loop;
-    }
+			i += insert.length-1; //Don't scan within the comment
+			continue loop;
+		}
 
-    // Color multi line comments
-    if (res[i] == '/' && res[i+1] == '*'){
+		// Color multi line comments
+		if (res[i] == '/' && res[i+1] == '*'){
 
-      scan:
-      for (j=i; j<res.length; j++){
-        if (res[j] == '*' && res[j+1] == '/'){
-          j += 2;
-          break scan;
-        }
-      }
+			scan:
+			for (j=i; j<res.length; j++){
+				if (res[j] == '*' && res[j+1] == '/'){
+					j += 2;
+					break scan;
+				}
+			}
 
-      let insert = `<span class="com">${res.slice(i, j)}</span>`;
-      res = res.slice(0, i) + insert + res.slice(j);
+			let insert = `<span class="com">${res.slice(i, j)}</span>`;
+			res = res.slice(0, i) + insert + res.slice(j);
 
-      i += insert.length-1; //Don't scan within the comment
-      continue loop;
-    }
+			i += insert.length-1; //Don't scan within the comment
+			continue loop;
+		}
 
-    // Highlight functions
-    if (isLetter(res[i]) && (res[i-1] == undefined || !isLetter(res[i-1]))){
-      scan: for (let j=i+1; j<res.length; j++){
-        if (res[j] == '('){
-          let insert = '<span class="cal">'+res.slice(i,j)+'</span>';
-          res = res.slice(0, i) + insert + res.slice(j);
-          i += insert.length-1;
+		// Highlight functions
+		if (isLetter(res[i]) && (res[i-1] == undefined || !isLetter(res[i-1]))){
+			scan: for (let j=i+1; j<res.length; j++){
+				if (res[j] == '('){
+					let insert = '<span class="cal">'+res.slice(i,j)+'</span>';
+					res = res.slice(0, i) + insert + res.slice(j);
+					i += insert.length-1;
 
-          continue loop;
-        }
+					continue loop;
+				}
 
-        if (!isLetter(res[j])){
-          break scan;
-        }
-      }
-    }
+				if (!isLetter(res[j])){
+					break scan;
+				}
+			}
+		}
 
-    // Single quote string
-    if (res[i] == "'"){
-      scan: for(let j=i+1; j<res.length; j++){
-        if (res[j] == '\n'){
-          break scan;
-        }
+		// Single quote string
+		if (res[i] == "'"){
+			scan: for(let j=i+1; j<res.length; j++){
+				if (res[j] == '\n'){
+					break scan;
+				}
 
-        if (res[j] == "'"){
-          insert = '<span class="str">'+res.slice(i, j+1)+'</span>';
-          res = res.slice(0,i)+insert+res.slice(j+1);
-          i += insert.length;
-          continue loop;
-        }
-      }
-    }
-    // Multi-lined string
-    if (res[i] == "`"){
-      scan: for(let j=i+1; j<res.length; j++){
-        if (res[j] == "`"){
-          insert = '<span class="str">'+res.slice(i, j+1)+'</span>';
-          res = res.slice(0,i)+insert+res.slice(j+1);
-          i += insert.length;
-          continue loop;
-        }
-      }
-    }
-    // Double qoute string
-    if (res[i] == '"'){
-      scan: for(let j=i+1; j<res.length; j++){
-        if (res[j] == '\n'){
-          break scan;
-        }
+				if (res[j] == "'"){
+					insert = '<span class="str">'+res.slice(i, j+1)+'</span>';
+					res = res.slice(0,i)+insert+res.slice(j+1);
+					i += insert.length;
+					continue loop;
+				}
+			}
+		}
+		// Multi-lined string
+		if (res[i] == "`"){
+			scan: for(let j=i+1; j<res.length; j++){
+				if (res[j] == "`"){
+					insert = '<span class="str">'+res.slice(i, j+1)+'</span>';
+					res = res.slice(0,i)+insert+res.slice(j+1);
+					i += insert.length;
+					continue loop;
+				}
+			}
+		}
+		// Double qoute string
+		if (res[i] == '"'){
+			scan: for(let j=i+1; j<res.length; j++){
+				if (res[j] == '\n'){
+					break scan;
+				}
 
-        if (res[j] == '"'){
-          insert = '<span class="str">'+res.slice(i, j+1)+'</span>';
-          res = res.slice(0,i)+insert+res.slice(j+1);
-          i += insert.length;
-          continue loop;
-        }
-      }
-    }
+				if (res[j] == '"'){
+					insert = '<span class="str">'+res.slice(i, j+1)+'</span>';
+					res = res.slice(0,i)+insert+res.slice(j+1);
+					i += insert.length;
+					continue loop;
+				}
+			}
+		}
 
-    // Color specific words
-    type: for (let type in highlight){
-      word: for (let key of highlight[type]){
-        key = key.toLowerCase();
+		// Color specific words
+		type: for (let type in highlight){
+			word: for (let key of highlight[type]){
+				key = key.toLowerCase();
 
-        // If the word is too long for the remaining scan, don't check it
-        if (i+key.length >= res.length){
-          continue word;
-        }
+				// If the word is too long for the remaining scan, don't check it
+				if (i+key.length >= res.length){
+					continue word;
+				}
 
-        let isWord = isLetter(key[0]);
+				let isWord = isLetter(key[0]);
 
-        // If this is not the start of a word
-        if (isWord && ( res[i-1]!=undefined && isLetter(res[i-1]) )){
-          continue word;
-        }
-        scan: for (j=0; j<key.length; j++){
-          if (res[i+j].toLowerCase() != key[j]){
-            continue word;
-          }
-        }
-        // If the end of this word isn't the end of the goal word
-        if (isWord && (res[j+1]!=undefined && isLetter(res[i+j]))){
-          continue word;
-        }
+				// If this is not the start of a word
+				if (isWord && ( res[i-1]!=undefined && isLetter(res[i-1]) )){
+					continue word;
+				}
+				scan: for (j=0; j<key.length; j++){
+					if (res[i+j].toLowerCase() != key[j]){
+						continue word;
+					}
+				}
+				// If the end of this word isn't the end of the goal word
+				if (isWord && (res[j+1]!=undefined && isLetter(res[i+j]))){
+					continue word;
+				}
 
-        // Word passed scan
-        let insert = `<span class="${type}">${res.slice(i, i+j)}</span>`;
-        res = res.slice(0, i) + insert + res.slice(i+j);
+				// Word passed scan
+				let insert = `<span class="${type}">${res.slice(i, i+j)}</span>`;
+				res = res.slice(0, i) + insert + res.slice(i+j);
 
-        i += insert.length - j; //Do not scan the new content
-        continue loop;
-      }
-    }
-  }
+				i += insert.length - j; //Do not scan the new content
+				continue loop;
+			}
+		}
+	}
 
-  // Make spacing exact
-  let escaping = false;
-  scan: for (let i=0; i<res.length; i++){
-    if (res[i] == '<'){
-      escaping = true;
-    }else if (res[i] == '>'){
-      escaping = false;
-    }
+	// Make spacing exact
+	let escaping = false;
+	scan: for (let i=0; i<res.length; i++){
+		if (res[i] == '<'){
+			escaping = true;
+		}else if (res[i] == '>'){
+			escaping = false;
+		}
 
-    if (escaping){
-      continue scan;
-    }
+		if (escaping){
+			continue scan;
+		}
 
-    if (res[i] == ' '){
-      res = res.slice(0, i) + '&nbsp' + res.slice(i+1);
-      i+= 4;
-    }
-  }
+		if (res[i] == ' '){
+			res = res.slice(0, i) + '&nbsp' + res.slice(i+1);
+			i+= 4;
+		}
+	}
 
-  res = res.replace(/\n/g, '<br>').replace(/\t/g, '&nbsp&nbsp'); // Encode tabs and spaces
-  return `<code>${res}</code>`;
+	res = res.replace(/\n/g, '<br>').replace(/\t/g, '&nbsp&nbsp'); // Encode tabs and spaces
+	return `<code>${res}</code>`;
 }
 
 
 module.exports = {
-  encode
+	encode
 };
--- bin/math.js
+++ bin/math.js
@@ -1,262 +1,262 @@
 function encode(input, nested){
-  input = input.replace(/<=/g, '≤').replace(/>=/g, '≥').replace(/<</g, '≪').replace(/>>/g, '≫')
-    .replace(/\+\/-/g, '±').replace(/-\/\+/g, '∓');
+	input = input.replace(/<=/g, '≤').replace(/>=/g, '≥').replace(/<</g, '≪').replace(/>>/g, '≫')
+		.replace(/\+\/-/g, '±').replace(/-\/\+/g, '∓');
 
-  let insert = '';
-  let dep = 0; // Depth
-  let a = 0;
-  let b = 0;
-  let c = 0;
-  let d = 0;
+	let insert = '';
+	let dep = 0; // Depth
+	let a = 0;
+	let b = 0;
+	let c = 0;
+	let d = 0;
 
-  let s = -1;
-  
-  scan:
-  for (let i=0; i<input.length; i++){
-    // Resolve unhandled nests
-    if (s != -1 && dep == 0 && input[i-1] == ')'){
-      i -= 1;
+	let s = -1;
+	
+	scan:
+	for (let i=0; i<input.length; i++){
+		// Resolve unhandled nests
+		if (s != -1 && dep == 0 && input[i-1] == ')'){
+			i -= 1;
 
-      let insert = '('+ encode(input.slice(s+1, i), true);
-      input = input.slice(0, s) + insert + input.slice(i);
-      i = (s) + insert.length; // Move scanner to the end of the insert
-      s = -1; // Make this braket resolved
+			let insert = '('+ encode(input.slice(s+1, i), true);
+			input = input.slice(0, s) + insert + input.slice(i);
+			i = (s) + insert.length; // Move scanner to the end of the insert
+			s = -1; // Make this braket resolved
 
-      continue scan;
-    }
+			continue scan;
+		}
 
-    // Only work at the current depth
-    switch (input[i]){
-      case '(':
-        if (dep == 0){
-          s = i;
-        }
+		// Only work at the current depth
+		switch (input[i]){
+			case '(':
+				if (dep == 0){
+					s = i;
+				}
 
-        dep += 1;
-        continue scan;
-      case ')':
-        dep -= 1;
-        continue scan;
-    }
+				dep += 1;
+				continue scan;
+			case ')':
+				dep -= 1;
+				continue scan;
+		}
 
-    if (dep !== 0){
-      continue scan;
-    }
+		if (dep !== 0){
+			continue scan;
+		}
 
-    switch (input[i]){
-      case '\\':
-        let hasLine = input[i+1] == '\\' ? 't' : 'f';
+		switch (input[i]){
+			case '\\':
+				let hasLine = input[i+1] == '\\' ? 't' : 'f';
 
-        // End the end of the numerator
-        search:
-        for (b=i; b>0; b--){
-          if (input[b] == ')'){
-            break search;
-          }
-        }
+				// End the end of the numerator
+				search:
+				for (b=i; b>0; b--){
+					if (input[b] == ')'){
+						break search;
+					}
+				}
 
-        // Find the beginning of the numerator
-        dep = 0;
-        search:
-        for (a=b; a>0; a--){
-          if (input[a] == ')'){
-            dep += 1;
-          }
+				// Find the beginning of the numerator
+				dep = 0;
+				search:
+				for (a=b; a>0; a--){
+					if (input[a] == ')'){
+						dep += 1;
+					}
 
-          if (input[a] == '('){
-            dep -= 1;
+					if (input[a] == '('){
+						dep -= 1;
 
-            if (dep === 0){
-              break search;
-            }
-          }
-        }
+						if (dep === 0){
+							break search;
+						}
+					}
+				}
 
-        // Find the beginning of the denominator
-        search:
-        for (c=i; c<input.length; c++){
-          if (input[c] == '('){
-            break search;
-          }
-        }
+				// Find the beginning of the denominator
+				search:
+				for (c=i; c<input.length; c++){
+					if (input[c] == '('){
+						break search;
+					}
+				}
 
-        // Find the end of the denominator
-        dep = 0;
-        search:
-        for (d=c; d<input.length; d++){
-          if (input[d] == '('){
-            dep += 1;
-          }else if (input[d] == ')'){
-            dep -= 1;
+				// Find the end of the denominator
+				dep = 0;
+				search:
+				for (d=c; d<input.length; d++){
+					if (input[d] == '('){
+						dep += 1;
+					}else if (input[d] == ')'){
+						dep -= 1;
 
-            if (dep === 0){
-              break search;
-            }
-          }
-        }
+						if (dep === 0){
+							break search;
+						}
+					}
+				}
 
-        a += 1;
-        c += 1;
+				a += 1;
+				c += 1;
 
-        insert = `<span class="fract" w="${hasLine}"><sup>${input.slice(a, b)}</sup><sub>${encode(input.slice(c, d))}</sub></span>`;
-        input = input.slice(0, a-1) + insert + input.slice(d+1);
-        i = a + insert.length-2;
+				insert = `<span class="fract" w="${hasLine}"><sup>${input.slice(a, b)}</sup><sub>${encode(input.slice(c, d))}</sub></span>`;
+				input = input.slice(0, a-1) + insert + input.slice(d+1);
+				i = a + insert.length-2;
 
-        continue scan;
-    
-      case '^':
-        // End the end of the numerator
-        search:
-        for (b=i; b>0; b--){
-          if (input[b] == ')'){
-            break search;
-          }
-        }
+				continue scan;
+		
+			case '^':
+				// End the end of the numerator
+				search:
+				for (b=i; b>0; b--){
+					if (input[b] == ')'){
+						break search;
+					}
+				}
 
-        // Find the beginning of the numerator
-        dep = 0;
-        search:
-        for (a=b; a>0; a--){
-          if (input[a] == ')'){
-            dep += 1;
-          }
+				// Find the beginning of the numerator
+				dep = 0;
+				search:
+				for (a=b; a>0; a--){
+					if (input[a] == ')'){
+						dep += 1;
+					}
 
-          if (input[a] == '('){
-            dep -= 1;
+					if (input[a] == '('){
+						dep -= 1;
 
-            if (dep === 0){
-              break search;
-            }
-          }
-        }
+						if (dep === 0){
+							break search;
+						}
+					}
+				}
 
-        // Find the beginning of the denominator
-        search:
-        for (c=i; c<input.length; c++){
-          if (input[c] == '('){
-            break search;
-          }
-        }
+				// Find the beginning of the denominator
+				search:
+				for (c=i; c<input.length; c++){
+					if (input[c] == '('){
+						break search;
+					}
+				}
 
-        // Find the end of the denominator
-        dep = 0;
-        search:
-        for (d=c; d<input.length; d++){
-          if (input[d] == '('){
-            dep += 1;
-          }else if (input[d] == ')'){
-            dep -= 1;
+				// Find the end of the denominator
+				dep = 0;
+				search:
+				for (d=c; d<input.length; d++){
+					if (input[d] == '('){
+						dep += 1;
+					}else if (input[d] == ')'){
+						dep -= 1;
 
-            if (dep === 0){
-              break search;
-            }
-          }
-        }
+						if (dep === 0){
+							break search;
+						}
+					}
+				}
 
-        a += 1;
-        c += 1;
+				a += 1;
+				c += 1;
 
-        insert = `<span class="pow"><span class="base">${input.slice(a, b)}</span><sup>${encode(input.slice(c, d), true)}</sup></span>`;
-        input = input.slice(0, a-1) + insert + input.slice(d+1);
-        i = a + insert.length-2;
+				insert = `<span class="pow"><span class="base">${input.slice(a, b)}</span><sup>${encode(input.slice(c, d), true)}</sup></span>`;
+				input = input.slice(0, a-1) + insert + input.slice(d+1);
+				i = a + insert.length-2;
 
-        continue scan;    
+				continue scan;    
 
-      case '/':
-        input = input.slice(0,i) + '<span class="opper">÷</span>' + input.slice(i+1);
-        i += 27;
-        continue scan;
-      case '*':
-        input = input.slice(0,i) + '<span class="opper">×</span>' + input.slice(i+1);
-        i += 27;
-        continue scan;
-      case '+':
-        input = input.slice(0,i) + '<span class="opper">+</span>' + input.slice(i+1);
-        i += 27;
-        continue scan;
-      case '-':
-        input = input.slice(0,i) + '<span class="opper">-</span>' + input.slice(i+1);
-        i += 27;
-        continue scan;
-      case '%':
-        input = input.slice(0,i) + '<span class="opper">%</span>' + input.slice(i+1);
-        i += 27;
-        continue scan;
-      case '&':
-        input = input.slice(0,i) + '<span class="opper">&</span>' + input.slice(i+1);
-        i += 27;
-        continue scan;
-      case '=':
-        input = input.slice(0,i) + '<span class="opper">=</span>' + input.slice(i+1);
-        i += 27;
-        continue scan;
-      case '!':
-        input = input.slice(0,i) + '<span class="opper">÷</span>' + input.slice(i+1);
-        i += 27;
-        continue scan;
-      case '<':
-        input = input.slice(0,i) + '<span class="opper">&gt</span>' + input.slice(i+1);
-        i += 29;
-        continue scan;
-      case '>':
-        input = input.slice(0,i) + '<span class="opper">&lt</span>' + input.slice(i+1);
-        i += 29;
-        continue scan;
-      case '≈':
-        input = input.slice(0,i) + '<span class="opper">≈</span>' + input.slice(i+1);
-        i += 27;
-        continue scan;
-      case ' ':
-        input = input.slice(0, i) + input.slice(i+1);
-        i--;
-        continue;
-    
-      case 's':
-        if (input.slice(i, i+5) == 'sqrt('){
-          a = i+5;
+			case '/':
+				input = input.slice(0,i) + '<span class="opper">÷</span>' + input.slice(i+1);
+				i += 27;
+				continue scan;
+			case '*':
+				input = input.slice(0,i) + '<span class="opper">×</span>' + input.slice(i+1);
+				i += 27;
+				continue scan;
+			case '+':
+				input = input.slice(0,i) + '<span class="opper">+</span>' + input.slice(i+1);
+				i += 27;
+				continue scan;
+			case '-':
+				input = input.slice(0,i) + '<span class="opper">-</span>' + input.slice(i+1);
+				i += 27;
+				continue scan;
+			case '%':
+				input = input.slice(0,i) + '<span class="opper">%</span>' + input.slice(i+1);
+				i += 27;
+				continue scan;
+			case '&':
+				input = input.slice(0,i) + '<span class="opper">&</span>' + input.slice(i+1);
+				i += 27;
+				continue scan;
+			case '=':
+				input = input.slice(0,i) + '<span class="opper">=</span>' + input.slice(i+1);
+				i += 27;
+				continue scan;
+			case '!':
+				input = input.slice(0,i) + '<span class="opper">÷</span>' + input.slice(i+1);
+				i += 27;
+				continue scan;
+			case '<':
+				input = input.slice(0,i) + '<span class="opper">&gt</span>' + input.slice(i+1);
+				i += 29;
+				continue scan;
+			case '>':
+				input = input.slice(0,i) + '<span class="opper">&lt</span>' + input.slice(i+1);
+				i += 29;
+				continue scan;
+			case '≈':
+				input = input.slice(0,i) + '<span class="opper">≈</span>' + input.slice(i+1);
+				i += 27;
+				continue scan;
+			case ' ':
+				input = input.slice(0, i) + input.slice(i+1);
+				i--;
+				continue;
+		
+			case 's':
+				if (input.slice(i, i+5) == 'sqrt('){
+					a = i+5;
 
-          // Find the end of the sqrt
-          dep = 0;
-          search:
-          for (b=a-1; b<input.length; b++){
-            if (input[b] == '('){
-              dep += 1;
-            }else if (input[b] == ')'){
-              dep -= 1;
+					// Find the end of the sqrt
+					dep = 0;
+					search:
+					for (b=a-1; b<input.length; b++){
+						if (input[b] == '('){
+							dep += 1;
+						}else if (input[b] == ')'){
+							dep -= 1;
 
-              if (dep == 0){
-                break search;
-              }
-            }
-          }
-          dep = 0;
+							if (dep == 0){
+								break search;
+							}
+						}
+					}
+					dep = 0;
 
-          insert = `√<span class="sqrt">${encode(input.slice(a, b))}</span>`;
-          input = input.slice(0, i) + insert + input.slice(b+1);
-          i += insert.length-1;
+					insert = `√<span class="sqrt">${encode(input.slice(a, b))}</span>`;
+					input = input.slice(0, i) + insert + input.slice(b+1);
+					i += insert.length-1;
 
-          continue scan;
-        }
-    }
+					continue scan;
+				}
+		}
 
-    // Must be a number / or algebra
-    input = input.slice(0,i) + '<span class="char">'+input[i]+'</span>' + input.slice(i+1);
-    i += 26;
-    continue scan;
-  }
+		// Must be a number / or algebra
+		input = input.slice(0,i) + '<span class="char">'+input[i]+'</span>' + input.slice(i+1);
+		i += 26;
+		continue scan;
+	}
 
-  return input;
+	return input;
 }
 
 module.exports = {
-  encode: (input)=>{
-    input = input.split('\n');
-    let res = '';
+	encode: (input)=>{
+		input = input.split('\n');
+		let res = '';
 
-    for (let eq of input){
-      res += `<span class="row">${encode(eq)}</span>`;
-    }
+		for (let eq of input){
+			res += `<span class="row">${encode(eq)}</span>`;
+		}
 
-    return res;
-  }
+		return res;
+	}
 }
